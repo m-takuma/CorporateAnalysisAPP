1ae0576f9d5e89087c2f6308b54e1328d52061e5
M_Takuma <m.takuma.dev@gmail.com>
2022-10-02 22:28:33 +0900

    テスト追加 APITests

---
+++ CorporateAnalysisHubAPPTests/Model/APITests.swift
@@ -0,0 +1,31 @@
+//
+//  APITests.swift
+//  CorporateAnalysisHubAPPTests
+//
+//  Created by M_Taku on 2022/10/02.
+//
+
+@testable import CorporateAnalysisHubAPP
+import XCTest
+import gRPC_Core
+
+class APITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testCompanyFind() async throws {
+        let res = try? await companyFind(q: "9983",
+                                                type: .sec_code,
+                                                maxResults: 100)
+        let metaData = MetaData(count: 1, q: "9983", type: CompanySearchType.sec_code.rawValue, maxResults: 100)
+        let apiCompany = ApiCompany(num: 1, jcn: "9250001000684", edinet_code: "E03217", sec_code: "9983", name_jp: "ファーストリテイリング", name_eng: "FAST RETAILING CO., LTD.")
+        let expectValue = CompanyResponse(metadata: metaData, results: [apiCompany])
+        XCTAssertEqual(res, expectValue)
+    }
+}
