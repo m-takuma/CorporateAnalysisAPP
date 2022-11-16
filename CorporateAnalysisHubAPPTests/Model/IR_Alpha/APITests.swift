96441bff80c70606bf350a6b6e17a8f04bc364c5
M_Takuma <m.takuma.dev@gmail.com>
2022-11-17 01:07:48 +0900

    テストコード修正

--- CorporateAnalysisHubAPPTests/Model/IR_Alpha/APITests.swift
+++ CorporateAnalysisHubAPPTests/Model/IR_Alpha/APITests.swift
@@ -11,8 +11,6 @@ import gRPC_Core
 
 class APITests: XCTestCase {
 
-    private let api = IR_Alpha()
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -22,7 +20,7 @@ class APITests: XCTestCase {
     }
 
     func test_証券コードを指定して一つの結果が得られる() async throws {
-        let res = try? await api.companyFind(q: "9983",
+        let res = try? await IR_Alpha.companyFind(q: "9983",
                                                 type: .sec_code,
                                                 maxResults: 100)
         let metaData = MetaData(count: 1, q: "9983", type: IR_Alpha.CompanySearchType.sec_code.rawValue, maxResults: 100)
